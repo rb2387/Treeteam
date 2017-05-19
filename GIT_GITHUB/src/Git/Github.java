a93972acbdd8212ecb8d89a5f91184f949c85574
Rohit Baweja <rbaweja2387@gmail.com>
2017-05-19 16:43:32 +0530

    First Change

--- GIT_GITHUB/src/Git/Github.java
+++ GIT_GITHUB/src/Git/Github.java
@@ -4,6 +4,13 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.gargoylesoftware.htmlunit.WebWindow;
+import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptExecutor;
+
 public class Github {
 
 	public void getProperties(){
@@ -15,10 +22,25 @@ public class Github {
 			String st = item.next();
 			System.out.printf("The property is %s: %s : %n ",st , System.getProperty(st));
 			
-			
+		}	
+	}
+		
+
+		public void setupserver(){
+			System.setProperty("webdriver.chrome.driver","/Users/rohitbaweja/Desktop/chromedriver");
+		 WebDriver driver = new ChromeDriver();
+		 ChromeOptions opt = new ChromeOptions();
+			opt.addArguments("disable-infobars");
+			opt.addArguments("diable-notification");
+			driver.get("http://www.google.com");	
+			driver.manage().window().maximize();
+			//JavaScriptExecutor sc = new JavaScriptExecutor(driver);	// TODO Auto-generated method stub
+				System.out.println("we are there");
+				System.exit(0);
 		}
 		
+		}	
+			
 		
-	}
 	
-}
+
